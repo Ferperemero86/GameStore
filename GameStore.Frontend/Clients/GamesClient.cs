bc06928305cbb9e2fc7cc78fbe24e4224802462f
Ferperemero86 <fernandoperezmerono@gmail.com>
2024-11-25 08:28:23 +0000

    Add edit game form

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -1,3 +1,4 @@
+
 using GameStore.Frontend.Models;
 
 namespace GameStore.Frontend.Clients;
@@ -28,5 +29,25 @@ public class GamesClient
     }
   ];
 
+  private readonly Genre[] genres = new GenresClient().GetGenres();
+
   public GameSummary[] GetGames() => [.. games];
+
+  public void AddGame(GameDetails game)
+  {
+    ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+
+    var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+
+    var gameSummary = new GameSummary
+    {
+      Id = games.Count + 1,
+      Name = game.Name,
+      Genre = genre.Name,
+      Price = game.Price,
+      ReleaseDate = game.ReleaseDate
+    };
+
+    games.Add(gameSummary);
+  }
 }
