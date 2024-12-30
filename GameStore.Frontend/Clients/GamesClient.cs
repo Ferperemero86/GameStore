80a9b1ba0ef751b784045563da753b387717ed4f
Ferperemero86 <fernandoperezmerono@gmail.com>
2024-12-30 08:02:07 +0000

    Add methods to update game

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -14,14 +14,14 @@ public class GamesClient
       ReleaseDate = new DateOnly(1992, 7, 15)
     },
     new() {
-      Id = 1,
+      Id = 2,
       Name = "Final Fantasy",
       Genre = "RPG", 
       Price = 15.00M,
       ReleaseDate = new DateOnly(2000, 7, 15)
     }, 
     new() {
-      Id = 1,
+      Id = 3,
       Name = "Silent Hill 2",
       Genre = "Horror", 
       Price = 30.00M,
@@ -35,19 +35,60 @@ public class GamesClient
 
   public void AddGame(GameDetails game)
   {
-    ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
-
-    var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+    Genre genre = GetGenreById(game.GenreId);
 
     var gameSummary = new GameSummary
+      {
+        Id = games.Count + 1,
+        Name = game.Name,
+        Genre = genre.Name,
+        Price = game.Price,
+        ReleaseDate = game.ReleaseDate
+      };
+
+        games.Add(gameSummary);
+  }
+
+  public GameDetails GetGame(int id)
+  {
+    GameSummary game = GetGameSummaryById(id);
+
+    var genre = genres.Single(genre => string.Equals(genre.Name, game.Genre, StringComparison.OrdinalIgnoreCase));
+
+    return new GameDetails
     {
-      Id = games.Count + 1,
-      Name = game.Name,
-      Genre = genre.Name,
-      Price = game.Price,
-      ReleaseDate = game.ReleaseDate
+        Id = game.Id,
+        Name = game.Name,
+        GenreId = genre.Id.ToString(),
+        Price = game.Price,
+        ReleaseDate = game.ReleaseDate
     };
 
-    games.Add(gameSummary);
+  }
+
+  public void UpdateGame(GameDetails updatedGame)
+  {
+    var genre = GetGenreById(updatedGame.GenreId);
+    GameSummary existingGame = GetGameSummaryById(updatedGame.Id);
+
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = genre.Name;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+  }
+
+  private GameSummary GetGameSummaryById(int id)
+  {
+    GameSummary? game = games.Find(game => game.Id == id);
+    ArgumentNullException.ThrowIfNull(game);
+
+    return game;
+  }
+
+  private Genre GetGenreById(string? id)
+  {
+    ArgumentException.ThrowIfNullOrWhiteSpace(id);
+
+    return genres.Single(genre => genre.Id == int.Parse(id));
   }
 }
--- GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2f842afceab403b7ac6b89d2c0a28521b7b7c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f010408ff3ffe131d7811999fa99ce4d42c30663")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
